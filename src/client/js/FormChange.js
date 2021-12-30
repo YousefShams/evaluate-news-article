69662ce85e3a7384b1b8a81210707ece322b0982
yousef shams <sunshams171@gmail.com>
2021-12-30 15:32:03 +0200

    edited urlPattern

--- src/client/js/FormChange.js
+++ src/client/js/FormChange.js
@@ -3,7 +3,7 @@ export function FormChange() {
         console.log("Changed");
         const btnElement=document.getElementById("btn");
         const urlText=inputElement.value;
-        const regexMatch=urlText.match(/http(s?)(:\/\/)((www\.)?)([a-zA-z0-9\-_]+)(\.(\w){3})((\/[a-zA-z0-9\-_]+)+)?/gim);
+        const regexMatch=urlText.match(/(http(s?)(:\/\/))?((www\.)?)([a-zA-z0-9-_]+)(\.(\w){3})(\/?)([a-zA-z0-9-_]+\/?)*/gim);
         if(regexMatch!=null){
             if(regexMatch[0]==urlText) {
                 inputElement.style.borderBottom="6px solid lightgreen";
--- src/client/js/FormSubmit.js
+++ src/client/js/FormSubmit.js
@@ -4,8 +4,9 @@ export function FormSubmit(e) {
     const btnElement=document.getElementById("btn");
     const urlText=inputElement.value;
     console.log("Clicked")
-    const regexMatch=urlText.match(/http(s?)(:\/\/)((www\.)?)([a-zA-z0-9\-_]+)(\.(\w){3})((\/[a-zA-z0-9\-_]+)+)?/gim);
-    if(regexMatch!=null){
+    const regexMatch=urlText.match(/(http(s?)(:\/\/))?((www\.)?)([a-zA-z0-9-_]+)(\.(\w){3})(\/?)([a-zA-z0-9-_]+\/?)*/gim);
+    
+    if(regexMatch!=null) {
         if (regexMatch[0]==urlText)
         Client.postReq(inputElement.value);
         else {
